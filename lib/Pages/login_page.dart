d02b70b2757eb517cec43c7422e6af0f002b80d2
Dhwanit07 <dhwanittadvi1307@hotmail.com>
2022-08-18 21:58:22 +0530

    Day 3 Completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -6,8 +6,13 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Material(
-        child: Center(
-      child: Text("Login Page"),
-    ));
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(
+              fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
